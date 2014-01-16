5dcac93f4816d5b4a964eea923b2f0bf738b1a8a
Carrot80 <kirsten@carrot-village.de>
2014-01-16 18:19:20 +0200

    50 Hz cleaning

--- Preprocessing/BIU/02_CorrectHB.m
+++ Preprocessing/BIU/02_CorrectHB.m
@@ -8,31 +8,45 @@ function ForAllPat ()
     
     for i = 1 : size (VolunteerList)
         if ( 0 == strcmp( VolunteerList(i,1).name, '.') && 0 == strcmp( VolunteerList(i,1).name, '..'))
-            BIUCorrectHB ( strcat(VolunteerFolder, VolunteerList(i,1).name), VolunteerList(i,1).name  ) ;
+            BIUCorrectHbLf ( strcat(VolunteerFolder, VolunteerList(i,1).name), VolunteerList(i,1).name  ) ;
         end
     end
 end
 
 
 
-function BIUCorrectHB (PatientPath, PatientName)
+function BIUCorrectHbLf (PatientPath, PatientName)
 
 % use this function if initial heartbeatcleaning did not work
 
  % Reject all other but ...
-        if ( 0 == strcmp (PatientPath, 'D:\kirsten_thesis\data\controls\zzz_md'))
+        if ( 0 == strcmp (PatientPath, 'D:\kirsten_thesis\data\controls\zzz_ca'))
             return;
         end
         
+    if exist(, 'file')
+        return
+    end        
+        
     Path                     = [];
     Path.DataInput           = strcat ( PatientPath, '\MEG\01_Input_noise_reduced')                 ;
-    Path.Preprocessing       = strcat ( PatientPath, '\MEG\02_PreProcessing')                       ;   
        
     fileName = strcat(Path.DataInput, filesep, 'n_c,rfhp0.1Hz');
     
     [cleanData,temp2e,period4,MCG,Rtopo]=correctHB(fileName,[], 1);
-    cd Path.DataInput
-    rewrite_pdf(cleanData,[],[],'xc,lf,hb') % error message here "bad pdf name", beachte 'pwd'
-
+    
+%     kh_rewrite_pdf(cleanData,[],'lf_n_c,rfhp0.1Hz','lf,hb') 
+    kh_rewrite_pdf(cleanData,[], fileName,'lf,hb') 
+    
+    [FourRef,Fref]=fftBasic(cleanData,round(sRate));
+    
+    
+    % Try Cleaning Train Frequency :
+    [cleanData,whereUp]=LFcleanNoCue('lf,hb_lf_n_c,rfhp0.1Hz',1017.25,'time', 'ADAPTIVE',16.67)
+    % Frequency Spectrum:
+    [FourRef,Fref]=fftBasic(var4Dref,round(sRate));
+    plot(FourRef, Fref)
+    
+    
 end
 
